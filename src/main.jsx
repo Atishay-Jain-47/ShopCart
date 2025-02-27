97da1da5c13cca18d59c172a97eafc3a0301ffb2
Atishay-Jain-47 <162458404+Atishay-Jain-47@users.noreply.github.com>
2025-02-27 23:39:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import {store} from './Redux/Store'
+import {store} from './redux/Store'
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
 
